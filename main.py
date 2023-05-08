3178a8ca7d41d12f555dc0a789e13779d9edf614
DrHagha <coby4313@gmail.com>
2023-05-09 07:32:30 +0900

    모델 생성 성공

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
+from starlette.middleware.cors import CORSMiddleware
+
 from models import Anniversary, Brand, Friend, Item, Member, Pay, Shipping, ShippingAddress, Wish
 from database import SessionLocal, engine
+
+from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
+
+from routes import MemberRouter
 
 Member.Base.metadata.create_all(bind=engine)
 Friend.Base.metadata.create_all(bind=engine)
@@ -18,11 +26,20 @@ Pay.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Your App Name",
+        version="0.1.0",
+        description="Your App Description",
+        routes=app.routes,
+    )
+    # Convert Pydantic models to serializable dictionaries
+    json_schema = jsonable_encoder(openapi_schema)
+    app.openapi_schema = json_schema
+    return app.openapi_schema
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.openapi = custom_openapi
+
+app.include_router(MemberRouter.router)
--- models/Member.py
+++ models/Member.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
+
+from datetime import datetime
 
 from database import Base
 
@@ -10,11 +12,14 @@ class Member(Base):
     pk = Column(Integer, primary_key=True, index=True)
     group_id = Column(Integer, index=True)
     id = Column(String, index=True)
-    pw = Column(String)
-    name = Column(String)
-    gender = Column(String)
-    birthday = Column(DateTime)
-    profile_image = Column(String)
+    pw = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    birthday = Column(DateTime, nullable=False)
+    profile_image = Column(String, nullable=True)
+    reg_date = Column(DateTime, nullable=False, default=datetime.now())
+    sec_date = Column(DateTime, nullable=True)
+    is_use = Column(Boolean, nullable=False, default=True)
 
     my_addresses = relationship("ShippingAddress", back_populates="owner")
     my_wish = relationship("Wish", back_populates="wishing_user")
